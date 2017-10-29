75d0f65e33f4965fb9f76671809512572c6cd021
Brent Payton <brent@brentpayton.com>
2017-10-29 16:20:10 +0000

    Changed column sorting on the main page.

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
+// -----------------------------------------------------------------------------
 // Confirmation dialogue for delete buttons
+// -----------------------------------------------------------------------------
 $(document).ready(function(){
   $(".delete").click(function(){
     if (!confirm("Do you want to delete")){
@@ -6,3 +8,30 @@ $(document).ready(function(){
     }
   });
 });
+
+// -----------------------------------------------------------------------------
+// Sort order toggles
+// -----------------------------------------------------------------------------
+function toggleTitle() {
+  if (window.location.href.indexOf("byTitleReverse") > -1) {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byTitle';
+  } else {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byTitleReverse';
+  }
+}
+
+function toggleAuthor() {
+  if (window.location.href.indexOf("byAuthorReverse") > -1) {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byAuthor';
+  } else {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byAuthorReverse';
+  }
+}
+
+function toggleDate() {
+  if (window.location.href.indexOf("byDateReverse") > -1) {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byDate';
+  } else {
+    window.location.href = 'https://dev.fifteenlines.com/poems/byDateReverse';
+  }
+}
